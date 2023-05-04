809327db11d9026094c310e2cadae129c1149e6e
22026024 <22026024@myrp.edu.sg>
2023-05-04 17:22:47 +0800

    commit

--- app/src/main/java/sg/edu/rp/c346/id22026024/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22026024/billplease/MainActivity.java
@@ -3,12 +3,29 @@ package sg.edu.rp.c346.id22026024.billplease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    RadioGroup radioPayment;
+    RadioButton radioDisplay;
+    ToggleButton toggleButton;
+    EditText etInput;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
